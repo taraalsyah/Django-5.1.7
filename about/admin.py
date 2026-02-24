280e1f2c2fdd06050cbba73a67c487b31de0eb90
Super User <root@srv1394391.hstgr.cloud>
2026-02-24 02:51:42 -0500

    Clean cache files from repo

--- about/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import AboutDb
-# Register your models here.
-
-admin.site.register([AboutDb])
--- about/wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-    'Django-5.1.7.settings'
-)
-
-application = get_wsgi_application()
